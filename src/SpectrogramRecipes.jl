3b472cfe260431ecfceeb9671aeb7f385cad3952
David MacMahon <davidm@astro.berkeley.edu>
2022-08-13 20:28:15 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/SpectrogramRecipes.jl
@@ -0,0 +1,5 @@
+module SpectrogramRecipes
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SpectrogramRecipes
+using Test
+
+@testset "SpectrogramRecipes.jl" begin
+    # Write your tests here.
+end
